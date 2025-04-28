efabe346ad4c70d5ca687a3d73c011553245c2de
oea0490 <1255275625@qq.com>
2025-04-28 23:07:44 +0800

    数组/字符串

---
+++ Interview150/ArrayAndString/MergeSortedArray.go
@@ -0,0 +1,24 @@
+package ArrayAndString
+
+func Merge(nums1 []int, m int, nums2 []int, n int) {
+	sorted := make([]int, 0, m+n)
+	p1, p2 := 0, 0
+	for {
+		if p1 == m {
+			sorted = append(sorted, nums2[p2:]...)
+			break
+		}
+		if p2 == n {
+			sorted = append(sorted, nums1[p1:]...)
+			break
+		}
+		if nums1[p1] < nums2[p2] {
+			sorted = append(sorted, nums1[p1])
+			p1++
+		} else {
+			sorted = append(sorted, nums2[p2])
+			p2++
+		}
+	}
+	copy(nums1, sorted)
+}
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"Leetcode/Interview150/ArrayAndString"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	nums1 := []int{1, 3, 5, 0, 0, 0}
+	nums2 := []int{2, 4, 6}
+	fmt.Println("nums1:", nums1)
+	fmt.Println("nums2:", nums2)
+	ArrayAndString.Merge(nums1, len(nums1)-len(nums2), nums2, len(nums2))
+	fmt.Println("merged:", nums1)
 }
