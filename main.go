db260f0120e4b1010aa7a7099fd308ba5b808930
songzhengyang.1101 <songzhengyang.1101@bytedance.com>
2025-04-29 11:36:02 +0800

    LC27.移除元素

--- main.go
+++ main.go
@@ -6,10 +6,9 @@ import (
 )
 
 func main() {
-	nums1 := []int{1, 3, 5, 0, 0, 0}
-	nums2 := []int{2, 4, 6}
-	fmt.Println("nums1:", nums1)
-	fmt.Println("nums2:", nums2)
-	ArrayAndString.Merge(nums1, len(nums1)-len(nums2), nums2, len(nums2))
-	fmt.Println("merged:", nums1)
+	nums := []int{3, 2, 2, 3}
+	val := 3
+	fmt.Println("before", nums)
+	ArrayAndString.RemoveElement(nums, val)
+	fmt.Println("after", nums)
 }
