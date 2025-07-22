9776e0fccdb9c1047daccdcfedeb4e32f924d15f
songzhengyang.1101 <songzhengyang.1101@bytedance.com>
2025-07-22 16:04:59 +0800

    46.全排列

---
+++ Hot100/Backtracking/Permutations.go
@@ -0,0 +1,29 @@
+package Backtracking
+
+func permute(nums []int) [][]int {
+	visited := make([]bool, len(nums))
+	ans := make([][]int, 0)
+	path := make([]int, 0)
+	dfs(nums, &ans, &path, &visited, 0)
+	return ans
+}
+
+func dfs(nums []int, ans *[][]int, path *[]int, visited *[]bool, cur int) {
+	if cur == len(nums) {
+		*ans = append(*ans, append([]int{}, *path...))
+		return
+	}
+	for i := 0; i < len(nums); i++ {
+		if (*visited)[i] {
+			continue
+		}
+		// 选择这一步
+		(*visited)[i] = true
+		*path = append(*path, nums[i])
+		// 搜索下一步
+		dfs(nums, ans, path, visited, cur+1)
+		// 恢复现场
+		(*visited)[i] = false
+		*path = (*path)[:len(*path)-1]
+	}
+}
