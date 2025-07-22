52641d770ae88072d03d4e4c41d101e8a8c4d82e
songzhengyang.1101 <songzhengyang.1101@bytedance.com>
2025-07-22 16:44:26 +0800

    70.爬楼梯

---
+++ Hot100/DynamicProgramming/ClimbingStairs.go
@@ -0,0 +1,16 @@
+package DynamicProgramming
+
+func climbStairs(n int) int {
+	// 定义dp[i]为爬到第i阶的方法数
+	// 状态转移方程：dp[i] = dp[i-1] + dp[i-2]
+	// 初始状态：dp[0] = 1, dp[1] = 1
+	dp := make([]int, n+1)
+	// 初始化
+	dp[0] = 1
+	dp[1] = 1
+	// 状态转移
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
