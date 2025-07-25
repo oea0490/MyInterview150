55bb8b517abab22ab947ce29efa28412a035dec1
oea0490 <1255275625@qq.com>
2025-07-26 00:23:12 +0800

    142.环形链表II

---
+++ Hot100/LinkedList/LinkedListCycleII.go
@@ -0,0 +1,24 @@
+package LinkedList
+
+func detectCycle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			break
+		}
+	}
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+	slow = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
+}
