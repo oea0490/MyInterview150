634f447c9b7f41d5d35e67b934fc894ed50aba3b
oea0490 <1255275625@qq.com>
2025-08-03 17:25:20 +0800

    Go输入输出

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-	"Leetcode/Interview150/ArrayAndString"
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	nums := []int{3, 2, 2, 3}
-	val := 3
-	fmt.Println("before", nums)
-	ArrayAndString.RemoveElement(nums, val)
-	fmt.Println("after", nums)
+	// 输入一行整数数组,用空格分开
+	fmt.Print("请输入一行整数(用空格分隔): ")
+	reader := bufio.NewReader(os.Stdin)
+	line, _ := reader.ReadString('\n')
+	line = strings.TrimSpace(line) // 去除换行符和空格
+	parts := strings.Split(line, " ")
+
+	// 转换为整数数组
+	intArr := make([]int, len(parts))
+	for i, part := range parts {
+		intArr[i], _ = strconv.Atoi(part)
+	}
+
+	fmt.Println("转换后的整数数组:", intArr)
 }
