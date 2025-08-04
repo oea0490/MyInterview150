f1511d11c74544b974365cd9c138026f6aed9acf
songzhengyang.1101 <songzhengyang.1101@bytedance.com>
2025-08-04 23:40:46 +0800

    104.二叉树的最大深度

---
+++ Hot100/BinaryTree/MaximumDepthOfBinaryTree.go
@@ -0,0 +1,10 @@
+package BinaryTree
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+	return max(leftDepth, rightDepth) + 1
+}
