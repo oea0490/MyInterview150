d9acaa71048617744dcb8fd03cad46eac7afda8c
songzhengyang.1101 <songzhengyang.1101@bytedance.com>
2025-08-05 13:22:03 +0800

    199.二叉树的右视图

---
+++ Hot100/BinaryTree/BinaryTreeRightSideView.go
@@ -0,0 +1,27 @@
+package BinaryTree
+
+func rightSideView(root *TreeNode) []int {
+	var ans []int
+	if root == nil {
+		return ans
+	}
+	var queue []*TreeNode
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if i == size-1 {
+				ans = append(ans, node.Val)
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+	return ans
+}
